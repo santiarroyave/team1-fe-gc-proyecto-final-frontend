6ebb7a2fa92faef9b3c8205a99166538e6610ecd
Jhon Junior del Águila Pinedo <jhonjr.dap@gmail.com>
2023-08-04 15:38:09 +0200

    feat(component:utils/oferta): add componente carta oferta, con listado de assets/db.json

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import db from '../../../assets/db.json'
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-
+  database: any = db;
 }
