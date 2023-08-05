91a78031bb4c4c743e9902a04191187ffeb6f8c3
santiarroyave <santi98cv@hotmail.com>
2023-08-05 22:38:32 +0200

    feat(crear-oferta): Crea la galeria de fotos

--- src/app/components/admin/crear-oferta/crear-oferta.component.ts
+++ src/app/components/admin/crear-oferta/crear-oferta.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class CrearOfertaComponent {
 
+  fotos:any = [];
+  constructor(){
+    this.fotos[0] = "https://concepto.de/wp-content/uploads/2015/03/paisaje-e1549600034372.jpg";
+    this.fotos[1] = "https://concepto.de/wp-content/uploads/2015/03/paisaje-e1549600034372.jpg";
+    this.fotos[2] = "https://concepto.de/wp-content/uploads/2015/03/paisaje-e1549600034372.jpg";
+    this.fotos[3] = "https://concepto.de/wp-content/uploads/2015/03/paisaje-e1549600034372.jpg";
+    this.fotos[4] = "https://concepto.de/wp-content/uploads/2015/03/paisaje-e1549600034372.jpg";
+    this.fotos[5] = "https://concepto.de/wp-content/uploads/2015/03/paisaje-e1549600034372.jpg";
+    this.fotos[6] = "https://concepto.de/wp-content/uploads/2015/03/paisaje-e1549600034372.jpg";
+  }
+
 }
