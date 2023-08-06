e5d9d0b57fb3371e4e3e7f5a44a7589073b0f510
santiarroyave <santi98cv@hotmail.com>
2023-08-07 01:56:56 +0200

    feat(header): Añade header y funcionalidades de scroll y ocultar navbar

--- src/app/components/home/header/header.component.ts
+++ src/app/components/home/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HostListener } from '@angular/core';
 
 @Component({
@@ -6,14 +6,24 @@ import { HostListener } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
 
   scroll:boolean = false;
+  navbarH:any;
 
+  // Está alerta de que se haga el scroll
   @HostListener("window:scroll", ['$event'])
   doSomethingOnWindowScroll($event:Event){
     this.navegarABuscador();
     this.scroll = true;
+  }
+  
+  ngOnInit(){
+    // Oculta el menu horizontal mientras se ve el header
+    if (this.scroll == false){
+      this.navbarH = document.querySelector("app-navbar-h");
+      this.navbarH.classList.add("d-none");
+    }
   }
   
   // Hace scroll hasta el buscador
@@ -21,6 +31,9 @@ export class HeaderComponent {
     let elemento = document.getElementById("buscador");
     if (elemento && this.scroll == false) {
       elemento.scrollIntoView({ behavior: 'smooth' });
+      
+      // Activa el menu horizontal
+      this.navbarH.classList.remove("d-none");
     }
   }
   
