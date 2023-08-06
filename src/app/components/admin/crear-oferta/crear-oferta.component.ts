3e7ab6a1e5355484091c64a5e0902298e9c48676
santiarroyave <santi98cv@hotmail.com>
2023-08-06 03:27:37 +0200

    fix(crear-oferta): Mejora el funcionamiento del ngIf para mostrar las actividades

--- src/app/components/admin/crear-oferta/crear-oferta.component.ts
+++ src/app/components/admin/crear-oferta/crear-oferta.component.ts
@@ -14,27 +14,22 @@ export class CrearOfertaComponent implements OnInit{
   nombreActividad:string="";
   descripcionActividad:string="";
 
-  constructor(){
-    this.fotos[0] = "https://concepto.de/wp-content/uploads/2015/03/paisaje-e1549600034372.jpg";
-    this.fotos[1] = "https://concepto.de/wp-content/uploads/2015/03/paisaje-e1549600034372.jpg";
-    this.fotos[2] = "https://concepto.de/wp-content/uploads/2015/03/paisaje-e1549600034372.jpg";
-    this.fotos[3] = "https://concepto.de/wp-content/uploads/2015/03/paisaje-e1549600034372.jpg";
-    this.fotos[4] = "https://concepto.de/wp-content/uploads/2015/03/paisaje-e1549600034372.jpg";
-    this.fotos[5] = "https://concepto.de/wp-content/uploads/2015/03/paisaje-e1549600034372.jpg";
-    this.fotos[6] = "https://concepto.de/wp-content/uploads/2015/03/paisaje-e1549600034372.jpg";
-
-    this.serviciosAlojamiento = ["Wifi", "Lavadora", "Aire acondicionado", "Cocina", "Secadora", "Calefacción", "Zona para trabajar", "Televisión", "Piscina", "Desayuno", "Gimnasio"];
-
-  }
+  constructor(){ }
   
   ngOnInit(): void {
+    // Generador de fotos de ejemplo
+    for (let i = 0; i < 7; i++) {
+      this.fotos[i] = "https://concepto.de/wp-content/uploads/2015/03/paisaje-e1549600034372.jpg";
+    }
+    
+    // Genera servicios para el alojamiento de ejemplo
+    this.serviciosAlojamiento = ["Wifi", "Lavadora", "Aire acondicionado", "Cocina", "Secadora", "Calefacción", "Zona para trabajar", "Televisión", "Piscina", "Desayuno", "Gimnasio"];
+
     // Generador de actividades de ejemplo
     for (let i = 0; i < 5; i++) {
       this.agregarActividad(`Actividad ${i}`, "Some quick example text to build on the card title and make up the bulk of the card's content. Some quick example text to build on the card title and make up the bulk of the card's content.", "https://www.portaventuraworld.com/blog/wp-content/uploads/2023/05/Paw-World-1200x600-1.jpg");
     }
 
-    // this.listaActividades = null;
-    
   }
 
   // Recoge los datos del formulario y los envia a la funcion que los agrega y resetea el formulario
