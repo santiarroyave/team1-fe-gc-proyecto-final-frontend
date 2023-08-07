6ad9f0d59f718a9df6554a6ecda347850875c443
santiarroyave <santi98cv@hotmail.com>
2023-08-07 04:07:10 +0200

    fix(home): Añade el navbar-v dentro del navbar-h

--- src/app/utils/navbar-h/navbar-h.component.ts
+++ src/app/utils/navbar-h/navbar-h.component.ts
@@ -13,13 +13,18 @@ export class NavbarHComponent {
       let menuV:any = document.getElementById("navbarVertical");
       let menuH:any = document.getElementById("navbarHorizontal");
       let menuV2:any = document.getElementById("menuV-2");
+      // Variable que permite hacer scroll en el menu ya que es position absolute
+      let contenedorFix:any = document.getElementById("contenedorFix");
   
+      
       if (menuV.classList.contains("show") || menuH.classList.contains("show")) {
         menuV.classList.remove("show");
         menuH.classList.remove("show");
+        contenedorFix.classList.remove("h-100", "overflow-scroll");
       } else {
         menuV.classList.add("show");
         menuH.classList.add("show");
+        contenedorFix.classList.add("h-100", "overflow-scroll");
       }
 
       // Oculta la barra blanca que aparecia en los moviles
