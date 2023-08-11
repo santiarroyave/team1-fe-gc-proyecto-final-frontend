fd7a7902dd06fb72c3c91f1a03aa18aa099d7abe
santiarroyave <santi98cv@hotmail.com>
2023-08-11 06:01:50 +0200

    feat(oferta-detalle): Añade galeria

--- src/app/components/oferta-detalle/oferta-detalle.component.ts
+++ src/app/components/oferta-detalle/oferta-detalle.component.ts
@@ -20,6 +20,8 @@ export class OfertaDetalleComponent implements OnInit {
   actividad: any = {};
 
   listaActividades:any = [];
+  fotosGaleria:any = [];
+  posicionFotoSeleccionada:number = 0;
 
   constructor(
     private route: ActivatedRoute,
@@ -38,6 +40,8 @@ export class OfertaDetalleComponent implements OnInit {
 
     // Genera actividades de ejemplo
     this.generadorListaActividades();
+    // Genera fotos para la galeria de ejemplo
+    this.generadorListaFotos();
   }
 
   toastTrigger(): void {
@@ -48,7 +52,7 @@ export class OfertaDetalleComponent implements OnInit {
   }
 
   generadorListaActividades(){
-    // Genera 10 iguales de ejemplo
+    // Genera 10 actividades iguales para usarlas de ejemplo
     for (let i = 0; i < 10; i++) {
       this.listaActividades.push({
         id: i,
@@ -56,8 +60,32 @@ export class OfertaDetalleComponent implements OnInit {
         descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto suscipit totam quam fugiat nemo magnam et perferendis distinctio cum? Quae earum ipsum aperiam, dolores quod iste non nisi mollitia eaque tempora rerum dolorum! Sunt optio amet repellat fuga quos aspernatur eum at impedit suscipit, quisquam voluptate est! Dolores, nam saepe.",
         imagen: "https://applications-media.feverup.com/image/upload/f_auto,w_550,h_550/fever2/plan/photo/0f30f6f2-d251-11ea-bf03-06551cb39bc6.jpg"
       })
-      
-      
     }
+  }
+
+  generadorListaFotos(){
+    for (let i = 0; i < 5; i++) {
+      this.fotosGaleria.push({
+        id: i,
+        url: "https://cdn.jumeirah.com/-/mediadh/dh/hospitality/jumeirah/hotels/dubai/burj-al-arab-jumeirah/hero/burj-al-arab-profile-exterior_6-4_landscape/burj-al-arab-profile-exterior_6-4_landscape__portrait.jpg?w=600"
+      });
+      this.fotosGaleria.push({
+        id: i,
+        url: "https://www.barcelo.com/guia-turismo/wp-content/uploads/2022/03/dubai-que-ver-pal.jpg"
+      });
+      this.fotosGaleria.push({
+        id: i,
+        url: "https://images.trvl-media.com/lodging/19000000/18660000/18658700/18658662/33793b1e.jpg?impolicy=resizecrop&rw=500&ra=fit"
+      });
+      this.fotosGaleria.push({
+        id: i,
+        url: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/19/6a/d1/18/dubai-suites.jpg?w=700&h=-1&s=1"
+      });
+    }
+  }
+
+  seleccionarImagenGaleria(id:number){
+    // Buscar posicion del id en la lista
+    this.posicionFotoSeleccionada; // Asignar posicion del id
   }
 }
